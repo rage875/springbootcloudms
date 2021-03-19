2bbff56021eb3e455d60465b090c728b9b3e2137
rage875 <rage.875@gmail.com>
2021-03-19 12:37:27 -0600

    mobile-app-ws: User controller CRUD defining operations added

---
+++ mobile-app-ws/src/main/java/com/restfulappsbuser/ms/mobileappws/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.restfulappsbuser.ms.mobileappws.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+	// Defining basic CRUD operations
+	@GetMapping
+	public String getUser() {
+		return "Get user was called";
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "Create user was called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "Update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "Delete user was called";
+	}
+}
