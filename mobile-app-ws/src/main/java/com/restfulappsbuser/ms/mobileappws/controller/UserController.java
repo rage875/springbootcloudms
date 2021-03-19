08619adf3e8d1a082fb081876460c7875963b428
rage875 <rage.875@gmail.com>
2021-03-19 13:04:56 -0600

    mobile-app-ws: Reading query string request, dev tool dependency

--- mobile-app-ws/src/main/java/com/restfulappsbuser/ms/mobileappws/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/restfulappsbuser/ms/mobileappws/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 	// Defining basic CRUD operations
+	@GetMapping()
+	public String getUsers(@RequestParam(value = "page") int page,
+			@RequestParam(value = "limit") int limit) {
+		return String.format("Get users was called: page:%d, limit:%d", page, limit);
+	}
+
 	@GetMapping(path="/{id}")
 	public String getUser(@PathVariable String id) {
 		return String.format("Get user was called %s", id);
