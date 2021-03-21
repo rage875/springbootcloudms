e4e202bf9a45b120209b71d9c6df78320b93915e
rage875 <rage.875@gmail.com>
2021-03-21 17:13:34 -0600

    mobile-app-ws: constructor based dependency injection

--- mobile-app-ws/src/main/java/com/restfulappsbuser/ms/mobileappws/userservice/imp/UserServiceImp.java
+++ mobile-app-ws/src/main/java/com/restfulappsbuser/ms/mobileappws/userservice/imp/UserServiceImp.java
@@ -2,17 +2,25 @@ package com.restfulappsbuser.ms.mobileappws.userservice.imp;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.restfulappsbuser.ms.mobileappws.model.reponse.UserRes;
 import com.restfulappsbuser.ms.mobileappws.model.request.UserDetailsRequestModel;
+import com.restfulappsbuser.ms.mobileappws.shared.Utils;
 import com.restfulappsbuser.ms.mobileappws.userservice.UserService;
 
 @Service
 public class UserServiceImp implements UserService{
 	private Map<String, UserRes> users;
+	private Utils utils;
+	
+	@Autowired
+	public UserServiceImp(Utils utils)
+	{
+		this.utils = utils;
+	}
 
 	@Override
 	public UserRes createUser(UserDetailsRequestModel userDetails) {
@@ -22,7 +30,7 @@ public class UserServiceImp implements UserService{
 		userRes.setLastName(userDetails.getLastName());
 		userRes.setEmail(userDetails.getEmail());
 		
-		String userId = UUID.randomUUID().toString();
+		String userId = utils.generateUserId();
 		userRes.setId(userId);
 
 		if(users == null) 
